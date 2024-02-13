3aab00ef7d96845299cb9ee2131730c8254958d5
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2024-02-13 00:42:11 -0700

    Added Chrome Types

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     entry: {
-        index: "./src/index.tsx"
+        index: "./src/index.tsx",
     },
     mode: "production",
     module: {
