35330eaf7d1e2e862435656101cde391a8406a9b
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2024-02-16 02:53:44 -0700

    Can now read text and decipher divs

--- src/injection/index.ts
+++ src/injection/index.ts
@@ -1,19 +1,70 @@
 /* global chrome */
-
-// window.alert("Hello World!")
+import './funnyStyles.sass'
 
 console.log("Script was injected.")
 
-// window.addEventListener("load", )
-document.addEventListener("keydown", (e) => {
-    if (e.key!==" ") return;
-    console.log("Script ran")
-
-    let x = document.activeElement;
-
-    if (x instanceof HTMLTextAreaElement||x instanceof HTMLInputElement||x instanceof HTMLDivElement) {
-        x.style.backgroundColor = "blue"
+const isTextareaOrInput = (element: Element | null) => {
+    if (!element) {
+        return false;
     }
-}, {capture: true})
+
+    const tagName = element.tagName.toUpperCase();
+  
+    if (tagName === "TEXTAREA" || tagName === "INPUT") {
+        return true;
+    }
+
+    const isContentEditable = element.getAttribute('contenteditable');
+  
+    if(isContentEditable) {
+        return true;
+    }
+  
+    return false;
+};
+
+const listenActiveElement = (callback: any) => {
+    // Initial check
+    let lastActiveElement = document.activeElement;
+    // Check if element is textarea or input
+    if (isTextareaOrInput(lastActiveElement)) {
+        callback(lastActiveElement);
+    }
+  
+    // Handle if focus changes
+    const detectFocus = () => {
+        if (lastActiveElement !== document.activeElement) {
+            lastActiveElement = document.activeElement;
+            if (isTextareaOrInput(lastActiveElement)) {
+                callback(lastActiveElement);
+            }
+        }
+    };
+  
+    window.addEventListener("focus", detectFocus, true);
+};
+
+const listenToTyping = (element: HTMLInputElement) => {
+    element.addEventListener("keyup", (e) => {
+        const target = e.target as HTMLInputElement|HTMLTextAreaElement|HTMLDivElement;
+        
+        // @ts-ignore
+        let text: string|undefined = target.value;
+  
+        // If the text is undefined (target.value not existing), then it is a content editable div
+        if(text === undefined) {
+            text = target.innerText;
+        }
+
+        if (text.toLowerCase().includes("gay")) {
+            target.classList.add('rainbow');
+        } else if (target.classList.contains('rainbow')) {
+            target.classList.remove('rainbow')
+        }
+        
+    });
+};
+
+listenActiveElement(listenToTyping);
 
 export {}
